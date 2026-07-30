9f661cfa18967189b223b93ef5293d9e28c45a68
NicolasCardosodaSilva <nicolas2006338@gmail.com>
2026-07-30 20:16:35 -0300

    inicio sobre vetores

---
+++ VetorEstatico/src/main/java/ads/esd/testes/ex1.java
@@ -0,0 +1,15 @@
+package ads.esd.testes;
+
+import ads.esd.VetorEstatico;
+
+public class ex1 {
+    static void main (){
+        VetorEstatico vetor = new VetorEstatico(3);
+            vetor.inserir("A");
+            vetor.inserir("B");
+            vetor.inserir("C");
+            vetor.inserir("D");
+
+            vetor.imprimir();
+    }
+}
---
+++ VetorEstatico/src/main/java/ads/esd/testes/ex2.java
@@ -0,0 +1,19 @@
+package ads.esd.testes;
+
+import ads.esd.VetorEstatico;
+
+public class ex2 {
+    static void main (){
+        VetorEstatico vetor = new VetorEstatico(10);
+        vetor.inserir("A");
+        vetor.inserir("B");
+        vetor.inserir("C");
+        vetor.inserir("F");
+
+        vetor.inserir(2,"w");
+        System.out.println(vetor.getTamanho());
+        vetor.removerFinal();
+        vetor.imprimir();
+    }
+}
+
