8ed6329fdca4d7a5ab904c8d861c00337d92b68a
NicolasCardosodaSilva <nicolas2006338@gmail.com>
2026-08-18 20:15:27 -0300

    vetor ordenado mal feito

---
+++ ATvetorAleatorio/src/main/java/ads/esd/Main.java
@@ -0,0 +1,11 @@
+package ads.esd;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        VetorOrdenado v = new VetorOrdenado<>(10);
+        v.OrdenarVetor();
+       v.imprimir();
+    }
+}
